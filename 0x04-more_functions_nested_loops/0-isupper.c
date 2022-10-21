468f1cfe1936d695bec063d8498533c73b383f7c
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-10-21 01:39:55 -0700

    Fix betty errors

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int _isupper(int c) {
+/*
+ * _isupper - check if a character is uppercase or not
+ *
+ * Return: 1 if c is uppercase, O otherwise
+ */
+int _isupper(int c)
+{
 	unsigned int result = isupper(c);
 
 	if (result == 0)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 }

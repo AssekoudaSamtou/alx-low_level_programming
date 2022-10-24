c2dc58b3f2a2f3223da7bb4598000c668b57ebe5
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-10-24 15:44:45 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string
+ *
+ * Return: length of s.
+ */
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}

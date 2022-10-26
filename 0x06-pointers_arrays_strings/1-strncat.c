d792f9777dc882829cecb381ec7253b607b7f986
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-10-26 11:02:33 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: number of bytes
+ *
+ * Return: a pointer to the resulting string @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        return (strncat(dest, src, n));
+}

32ec5a45dbab585d2e817a9f85dfaa40f9cd277f
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-11-03 07:13:45 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+int test(int n, int x);
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ *
+ * @n: int
+ *
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (test(n, n - 1));
+}
+
+/**
+ * test - test
+ *
+ * @n; int
+ *
+ * @x: int
+ */
+int test(int n, int x)
+{
+	if (x == 1)
+		return (1);
+
+	if (n % x == 0)
+		return (0);
+
+	return (test(n, x - 1));
+}

89d2f283eca53dea47a45527d67fc03ce3381e2c
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-11-14 17:29:36 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/*#ifndef struct dog**/
+/**
+ * struct dog - dog type
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**#endif ** ifndef struct dog */

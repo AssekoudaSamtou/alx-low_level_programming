b8166c51495ed99296aa97e485779ad92d475cab
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-11-25 11:33:41 -0800

    check nullity

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
-
-#ifndef LISTS_H
-#define LIST_H
 
 /**
  * struct list_s - singly linked list
@@ -14,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);

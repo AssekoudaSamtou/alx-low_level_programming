e24638c7967b01f8d75c9ed609a006886da16af7
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-11-25 09:21:07 -0800

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: The head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	do
+	{
+		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
+		count += 1;
+		h = h->next;
+	}
+	while (h != NULL);
+
+	return count;
+}

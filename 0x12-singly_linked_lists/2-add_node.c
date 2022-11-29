9671183e81f2fb6bb7c16d167f93f0f2e3def249
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-11-29 02:45:53 -0800

    check nullity

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,12 +16,18 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	new->str = strdup(str);
 	new->len = _strlen(new->str);
 	new->next = *head;
 
 	*head = new;
 
+	free(new->str);
+	free(new);
 	return (new);
 }
 

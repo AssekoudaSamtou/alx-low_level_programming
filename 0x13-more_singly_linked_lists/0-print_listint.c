623b9c4a0ab281b9e179bb136f8ca1866af879e7
Samtou Christophe Assekouda <christophe1999da@gmail.com>
2022-12-04 01:23:06 -0800

    1. List length

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: The head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h)
+	do {
+		printf("%d\n", h->n);
+		count += 1;
+		h = h->next;
+	} while (h != NULL);
+
+	return (count);
+}
